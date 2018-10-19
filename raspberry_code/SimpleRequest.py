3f8a11f43893e89a970700d6904c68e15a35e7bd
Anton Smerdov <neonx11ant@gmail.com>
2018-10-19 11:52:09 +0300

    Merge branch 'master' of https://github.com/Kichkun/smart_chair
    # Conflicts:
    #	raspberry_code/SimpleRequest.py

--- raspberry_code/SimpleRequest.py
+++ raspberry_code/SimpleRequest.py
@@ -57,9 +57,9 @@ class SimpleRequest:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--timestep_detect', type=float, default=1)
-    parser.add_argument('--timestep_send', type=float, default=3)
-    parser.add_argument('--max_time', type=float, default=9)
+    parser.add_argument('--timestep_detect', type=float, default=0.5)
+    parser.add_argument('--timestep_send', type=float, default=10)
+    parser.add_argument('--max_time', type=float, default=60)
     parser.add_argument('--verbose', type=bool, default=True)
     parser.add_argument('--label', type=str, default='')
     parser.add_argument('--meta', type=str, default='')
@@ -74,6 +74,9 @@ if __name__ == '__main__':
     timestep_send = args.timestep_send #  60  #  timestep between sendings
     max_time = args.max_time #  30 * 60  # total time of measurement
     verbose = args.verbose
+    label = args.label
+    peopleId = args.peopleId
+    meta = args.meta
 
     batch_size = int(timestep_send / timestep_detect)  # Количество измерений в одной отправке
     n_batches = int(max_time / timestep_send) # Количество отправок
@@ -93,10 +96,10 @@ if __name__ == '__main__':
                 print('Accelerometer data: ', data_accelerometer)
 
             ax, ay, az = data_magnetometer
-            simple_request.collectMagnetometer(ax, ay, az, 'MagLaBeL', 'meta-meta', 'peopleID', 'magnetometer')
+            simple_request.collectMagnetometer(ax, ay, az, label, meta, peopleId, 'magnetometer')
 
             ax, ay, az = data_accelerometer
-            simple_request.collectAccelerometer(ax, ay, az, 'AccLaBeL', 'meta-meta', 'peopleID', 'accelerometer')
+            simple_request.collectAccelerometer(ax, ay, az, label, meta, peopleId, 'accelerometer')
 
             time.sleep(timestep_detect)
 
